3e269824b37e37d55c53e6358a326cd6396b30f7
Vinny Barden <vinny.barden@capgemini.com>
2024-08-08 11:56:52 +0100

    ''

---
+++ src/Defra.Trade.Common.Function.Health/Extensions/HealthCheckResponseExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Azure.Management.ContainerService.Fluent.Models;
+
+namespace Defra.Trade.Common.Function.Health.Extensions;
+
+/// <summary>
+/// Extensions to help in dealing with Health Checks.
+/// </summary>
+[ExcludeFromCodeCoverage(Justification = "Namespace to be tested within and moved to nuget")]
+public static class HealthCheckResponseExtensions
+{
+    /// <summary>
+    /// Map the <see cref="HealthReport"/> received to the <see cref="HealthCheckResponse"/> to return.
+    /// </summary>
+    /// <param name="healthReport">The health data.</param>
+    /// <returns>The mapped response.</returns>
+    public static HealthCheckResponse ToResponse(this HealthReport healthReport)
+    {
+        return new HealthCheckResponse
+        {
+            Status = healthReport.Status.ToString(),
+            TotalDurationSeconds = healthReport.TotalDuration.TotalSeconds.ToString("F3"),
+            Results = healthReport.Entries.Select(e =>
+                new HealthCheckResponseEntry
+                {
+                    Name = e.Key,
+                    Description = e.Value.Description!,
+                    DurationSeconds = e.Value.Duration.TotalSeconds.ToString("F3"),
+                    Status = e.Value.Status.ToString(),
+                    Exception = e.Value.Exception?.Message!,
+                    Data = e.Value.Data
+                })
+        };
+    }
+}
---
+++ src/Defra.Trade.Common.Function.Health/HealthCheckResponse.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+namespace Defra.Trade.Common.Function.Health;
+
+/// <summary>
+/// Information about the health of a WebAPI.
+/// </summary>
+[ExcludeFromCodeCoverage(Justification = "Namespace to be tested within and moved to nuget")]
+public class HealthCheckResponse
+{
+    /// <summary>
+    /// The overall status of the health check.
+    /// </summary>
+    [JsonPropertyName("status")]
+    public string Status { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Amount of time that the response took to execute.
+    /// </summary>
+    public string TotalDurationSeconds { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Health check details.
+    /// </summary>
+    public IEnumerable<HealthCheckResponseEntry> Results { get; set; } = new List<HealthCheckResponseEntry>();
+}
